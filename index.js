e900fa10270ca7833af7ec43fd8e4ab8520481af
davi38 <davi4alves@gmail.com>
2022-09-11 19:52:42 -0300

    feat: pdf routes

--- index.js
+++ index.js
@@ -7,31 +7,30 @@ require('dotenv').config();
 const personRoutes = require('./routes/personRoutes')
 
 
+const pdfTemplate = require("./documents");
+
+
+
+
+
+
+
 //LER JSON
 app.use(express.urlencoded({
     extended: true,
 }))
-
 app.use(express.json())
 //Rotas API
-
-
+//#Person
 app.use('/person', personRoutes)
-
-
-
-
+//#Pdf
+app.use('/pdf', pdfTemplate)
 
 // ROTAS
 
 app.get('/', (req, res) => {
     res.json({ msg: 'Oi express' })
 })
-
-
-
-
-
 mongoose.connect(process.env.DB_URI)
     .then(() => {
         console.log('conectado ao mongo db')
